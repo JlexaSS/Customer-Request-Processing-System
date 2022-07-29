011b63de8260d10e05616e2cb6c97f94c07809f7
JlexaSS <jowe.jlexa@mail.ru>
2022-07-29 14:28:54 +0300

    feat: create project ProcessingSystem

---
+++ ProcessingSystem/src/main/java/org/example/ProcessingSystem/ProcessingSystemApplication.java
@@ -0,0 +1,11 @@
+package org.example.ProcessingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessingSystemApplication.class, args);
+    }
+}
