c5cb17b18ce3879bcdb13589a14c55b702822bee
JlexaSS <jowe.jlexa@mail.ru>
2022-07-29 12:36:58 +0300

    feat: create project SolutionStore

---
+++ SolutionStore/src/main/java/org/example/SolutionStore/SolutionStoreApplication.java
@@ -0,0 +1,11 @@
+package org.example.SolutionStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SolutionStoreApplication.class, args);
+    }
+}
