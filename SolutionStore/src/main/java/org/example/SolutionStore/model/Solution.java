cff950489003684a9ee8d2791250cae8647d29a7
JlexaSS <jowe.jlexa@mail.ru>
2022-07-29 13:00:34 +0300

    feat: add entity and db migration

---
+++ SolutionStore/src/main/java/org/example/SolutionStore/model/Solution.java
@@ -0,0 +1,28 @@
+package org.example.SolutionStore.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Solution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String typename;
+
+    @Column(nullable = false)
+    private String solution;
+}
